294c36ad63806e5e0c195e27b10755b1ff8a76cc
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-06 16:24:51 -0300

    first commit

---
+++ src/main/java/com/tidz/book_management_sys/BookManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.book_management_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/book_management_sys/BookManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.book_management_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
