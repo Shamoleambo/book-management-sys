153b3525933f3d429174d75a251172568bdbe23a
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-07 10:17:16 -0300

    implement getBooks method to BookController

--- src/main/java/com/tidz/book_management_sys/controller/BookController.java
+++ src/main/java/com/tidz/book_management_sys/controller/BookController.java
@@ -1,9 +1,14 @@
 package com.tidz.book_management_sys.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tidz.book_management_sys.entity.Book;
 import com.tidz.book_management_sys.service.BookService;
 
 @Controller
@@ -12,4 +17,11 @@ public class BookController {
 
 	@Autowired
 	private BookService bookService;
+
+	@GetMapping
+	public String getBooks(Model model) {
+		List<Book> books = this.bookService.getAllBooks();
+		model.addAttribute("books", books);
+		return "books";
+	}
 }
